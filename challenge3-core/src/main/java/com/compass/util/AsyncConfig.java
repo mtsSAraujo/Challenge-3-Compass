9d064c59f81c1840006b6a7eee23aa604dcff571
Mateus Silva Araujo <mtsSAraujo@users.noreply.github.com>
2023-08-13 20:59:03 -0300

    feat: add Async Configuration class to handle Async Requisitions

---
+++ challenge3-core/src/main/java/com/compass/util/AsyncConfig.java
@@ -0,0 +1,22 @@
+package com.compass.util;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig {
+
+    @Bean
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(20);
+        executor.setMaxPoolSize(25);
+        executor.initialize();
+        return executor;
+    }
+}
